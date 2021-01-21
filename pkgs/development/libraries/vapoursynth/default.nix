1c3e1458cca60f9b6cf8d2ee84b3d1725fe1df77
Simon Bruder <simon@sbruder.de>
2021-01-21 15:53:58 +0100

    vapoursynth: add withPlugins interface
    Since nixpkgs does not currently have an attribute set with plugins, the
    usage differes from python-style withPackages as it accepts a list
    instead of a function as a parameter.
    
    Using plugins in other programs that require vapoursynth (like `vsedit`)
    can be achieved by adding it to the plugin list (e.g.
    `vapoursynth.withPlugins [ vapoursynth-editor … ]`) or by passing the
    `VAPOURSYNTH_PLUGIN_PATH` variable.

--- pkgs/development/libraries/vapoursynth/default.nix
+++ pkgs/development/libraries/vapoursynth/default.nix
@@ -1,4 +1,5 @@
-{ lib, stdenv, fetchFromGitHub, pkg-config, autoreconfHook, makeWrapper
+{ buildEnv, lib, stdenv, fetchFromGitHub, pkg-config, autoreconfHook, makeWrapper
+, vapoursynth
 , zimg, libass, python3, libiconv
 , ApplicationServices
 , ocrSupport ?  false, tesseract ? null
@@ -21,6 +22,10 @@ stdenv.mkDerivation rec {
     sha256 = "1krfdzc2x2vxv4nq9kiv1c09hgj525qn120ah91fw2ikq8ldvmx4";
   };
 
+  patches = [
+    ./plugin-path-environment-variable.patch
+  ];
+
   nativeBuildInputs = [ pkg-config autoreconfHook makeWrapper ];
   buildInputs = [
     zimg libass
@@ -42,6 +47,38 @@ stdenv.mkDerivation rec {
     # wrapper, what python3 version was used to build vapoursynth so
     # the right python3.sitePackages will be used there.
     inherit python3;
+
+    withPlugins = plugins: let
+      pythonEnvironment = python3.buildEnv.override {
+        extraLibs = plugins;
+      };
+    in
+    buildEnv {
+      name = "${vapoursynth.name}-with-plugins";
+      paths = [ vapoursynth ] ++ plugins;
+      buildInputs = [ makeWrapper ];
+      postBuild = ''
+        # If vapoursynth is the only path providing a binary, buildEnv will
+        # link bin to the vapoursynth derivation, which is write protected.
+        # Then wrapProgram will not work, since it overwrites/adds files in
+        # bin.
+        mkdir $out/bin_
+        for binary in $out/bin/*; do
+            # skip wrapProgram original files
+            [[ $binary == .* ]] && continue
+
+            makeWrapper $(realpath $binary) $out/bin_/$(basename $binary) \
+                --set VAPOURSYNTH_PLUGIN_PATH $out/lib/vapoursynth \
+                --prefix PYTHONPATH : ${pythonEnvironment}/${python3.sitePackages}
+        done
+        rm -rf $out/bin
+        mv $out/bin_ $out/bin
+      '';
+
+      passthru = {
+        inherit python3;
+      };
+    };
   };
 
   postInstall = ''
@@ -54,7 +91,6 @@ stdenv.mkDerivation rec {
     homepage    = "http://www.vapoursynth.com/";
     license     = licenses.lgpl21;
     platforms   = platforms.x86_64;
-    maintainers = with maintainers; [ rnhmjoj tadeokondrak ];
+    maintainers = with maintainers; [ rnhmjoj sbruder tadeokondrak ];
   };
-
 }
